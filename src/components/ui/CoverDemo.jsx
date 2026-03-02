96de69d1ec30ded58ea496638543ff9bc7e9fc2a
Utkarsh Chaudhary <utkarshjaat01@gmail.com>
2026-03-02 23:19:20 +0530

    JJ

--- src/components/ui/CoverDemo.jsx
+++ src/components/ui/CoverDemo.jsx
@@ -7,7 +7,7 @@ export function CoverDemo() {
       <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
         Enroll today for just{" "}
         <span className="text-green-600 text-7xl font-bold align-middle">
-          $2499
+          $2999
         </span>
         {" "}and unlock
         <br />
